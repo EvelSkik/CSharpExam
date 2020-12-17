e5ef68dd6bcd4a4d24c023317fe9f93734bc0b5b
EvelSkik <33183514+EvelSkik@users.noreply.github.com>
2020-12-17 17:04:34 +0200

    Add files via upload

---
+++ FirstTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter text");
+            var text = Console.ReadLine();
+            Console.WriteLine(text.Substring(0, 1) + text + text.Substring(0, 1) );
+            
+        }
+    }
+}
